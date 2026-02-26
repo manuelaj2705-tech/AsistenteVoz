c6184b51fb93e2fbe0f7adccaac57af747a5e349
manuelaj2705-tech <manuelaj.27.05@gmail.com>
2026-02-26 08:08:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,14 +20,9 @@ except:
     pass
 
 st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
-         ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
-         ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
-         ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
-         '  '
-         ' Franz Kafka.'
-        
-        )
+st.write('Un asistente de voz es una tecnología que permite interactuar con dispositivos usando la voz.
+Reconoce lo que dices, lo interpreta y responde realizando acciones como buscar información, controlar dispositivos,
+enviar mensajes o dar recomendaciones, haciendo más fácil y rápida la interacción sin necesidad de usar las manos.' )
            
 st.markdown(f"Quieres escucharlo?, copia el texto")
 text = st.text_area("Ingrese El texto a escuchar.")
