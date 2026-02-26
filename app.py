fcc88eeb61d0147d49508a39ea4662fe8bc35034
manuelaj2705-tech <manuelaj.27.05@gmail.com>
2026-02-26 08:09:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import os
 import time
 import glob
-import os
 from gtts import gTTS
 from PIL import Image
 import base64
@@ -10,9 +9,9 @@ import base64
 st.title("Conversión de Texto a Audio")
 image = Image.open('gato_raton.png')
 st.image(image, width=350)
+
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
-
 
 try:
     os.mkdir("temp")
@@ -20,25 +19,28 @@ except:
     pass
 
 st.subheader("Una pequeña Fábula.")
-st.write('Un asistente de voz es una tecnología que permite interactuar con dispositivos usando la voz.
+st.write("""
+Un asistente de voz es una tecnología que permite interactuar con dispositivos usando la voz.
 Reconoce lo que dices, lo interpreta y responde realizando acciones como buscar información, controlar dispositivos,
-enviar mensajes o dar recomendaciones, haciendo más fácil y rápida la interacción sin necesidad de usar las manos.' )
-           
-st.markdown(f"Quieres escucharlo?, copia el texto")
+enviar mensajes o dar recomendaciones, haciendo más fácil y rápida la interacción sin necesidad de usar las manos.
+""")
+
+st.markdown("Quieres escucharlo?, copia el texto")
 text = st.text_area("Ingrese El texto a escuchar.")
 
-tld='com'
+tld = 'com'
 option_lang = st.selectbox(
     "Selecciona el lenguaje",
-    ("Español", "English"))
-if option_lang=="Español" :
-    lg='es'
-if option_lang=="English" :
-    lg='en'
+    ("Español", "English")
+)
 
-def text_to_speech(text, tld,lg):
-    
-    tts = gTTS(text,lang=lg) # tts = gTTS(text,'en', tld, slow=False)
+if option_lang == "Español":
+    lg = 'es'
+if option_lang == "English":
+    lg = 'en'
+
+def text_to_speech(text, tld, lg):
+    tts = gTTS(text, lang=lg)
     try:
         my_file_name = text[0:20]
     except:
@@ -46,35 +48,25 @@ def text_to_speech(text, tld,lg):
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name, text
 
-
-#display_output_text = st.checkbox("Verifica el texto")
-
 if st.button("convertir a Audio"):
-     result, output_text = text_to_speech(text, 'com',lg)#'tld
-     audio_file = open(f"temp/{result}.mp3", "rb")
-     audio_bytes = audio_file.read()
-     st.markdown(f"## Tú audio:")
-     st.audio(audio_bytes, format="audio/mp3", start_time=0)
+    result, output_text = text_to_speech(text, 'com', lg)
+    audio_file = open(f"temp/{result}.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.markdown("## Tú audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
 
-     #if display_output_text:
-     
-     #st.write(f" {output_text}")
-    
-#if st.button("ElevenLAabs",key=2):
-#     from elevenlabs import play
-#     from elevenlabs.client import ElevenLabs
-#     client = ElevenLabs(api_key="a71bb432d643bbf80986c0cf0970d91a", # Defaults to ELEVEN_API_KEY)
-#     audio = client.generate(text=f" {output_text}",voice="Rachel",model="eleven_multilingual_v1")
-#     audio_file = open(f"temp/{audio}.mp3", "rb")
+    with open(f"temp/{result}.mp3", "rb") as f:
+        data = f.read()
 
-     with open(f"temp/{result}.mp3", "rb") as f:
-         data = f.read()
-
-     def get_binary_file_downloader_html(bin_file, file_label='File'):
+    def get_binary_file_downloader_html(bin_file, file_label='File'):
         bin_str = base64.b64encode(data).decode()
         href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
         return href
-     st.markdown(get_binary_file_downloader_html("audio.mp3", file_label="Audio File"), unsafe_allow_html=True)
+
+    st.markdown(
+        get_binary_file_downloader_html("audio.mp3", file_label="Audio File"),
+        unsafe_allow_html=True
+    )
 
 def remove_files(n):
     mp3_files = glob.glob("temp/*mp3")
@@ -85,6 +77,5 @@ def remove_files(n):
             if os.stat(f).st_mtime < now - n_days:
                 os.remove(f)
                 print("Deleted ", f)
-
 
 remove_files(7)
